2bb452c4a5eaae157b4615049f4bbac01dd98924
pittuxx <pittuxx@gmail.com>
2015-10-21 20:01:57 +0200

    Initial commit

---
+++ db/schema.rb
@@ -0,0 +1,62 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20151014172052) do
+
+  create_table "iuris_contacts_contacts", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "company"
+    t.string   "email"
+    t.string   "phone"
+    t.text     "comments"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "iuris_contacts_contacts", ["user_id"], name: "index_iuris_contacts_contacts_on_user_id"
+
+  create_table "iuris_tasks_tasks", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.integer  "contact_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "state"
+  end
+
+  add_index "iuris_tasks_tasks", ["contact_id"], name: "index_iuris_tasks_tasks_on_contact_id"
+  add_index "iuris_tasks_tasks", ["user_id"], name: "index_iuris_tasks_tasks_on_user_id"
+
+  create_table "iuris_users", force: :cascade do |t|
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.boolean  "admin",                  default: false
+  end
+
+  add_index "iuris_users", ["email"], name: "index_iuris_users_on_email", unique: true
+  add_index "iuris_users", ["reset_password_token"], name: "index_iuris_users_on_reset_password_token", unique: true
+
+end
